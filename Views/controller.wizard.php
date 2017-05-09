78e9bdee716777ff2c02928e85a5590391a48562
ozanbote <ozanbote@windowslive.com>
2017-05-09 21:35:03 +0300

    1.0.0 -> Updated
    1.0.0 -> Updated

--- Views/controller.wizard.php
+++ Views/controller.wizard.php
@@ -1,15 +1,24 @@
 <div class="row">
     <div class="col-lg-12">
-
-            <div class="form-group">
-                <label>{{LANG['controllerName']}}</label>
-                @@Form::required()->class('form-control')->placeholder('Controllers/ControllerName')->text('controller'):
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title"><i class="fa fa-book fa-fw"></i> {{LANG['newController']}}</h3>
             </div>
+            <div class="panel-body">
 
-            <div class="form-group">
-                <label>{{LANG['functions']}}</label>
-                @@Form::class('form-control')->placeholder('Function1,Function2,Function3 ...')->text('functions'):
+                <div class="form-group">
+                    <label>{{LANG['controllerName']}}</label>
+                    @@Form::required()->class('form-control')->placeholder('Controllers/ControllerName')->text('controller'):
+                </div>
+
+                <div class="form-group">
+                    <label>{{LANG['functions']}}</label>
+                    @@Form::class('form-control')->placeholder('Function1,Function2,Function3 ...')->text('functions'):
+                </div>
+
+
             </div>
-
+        </div>
     </div>
+
 </div>
