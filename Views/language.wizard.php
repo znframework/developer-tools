394c7351a75e2511eaa3020849f9c7d735cfdd2c
ozanbote <ozanbote@windowslive.com>
2017-05-19 21:22:11 +0300

    2.0.0 -> Updated
    2.0.0 -> Updated

--- Views/language.wizard.php
+++ Views/language.wizard.php
@@ -2,7 +2,7 @@
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">
-            @@LANG['grids']: <small> {{LANG['overview']}}</small>
+            @@LANG['languages']: <small> {{LANG['overview']}}</small>
         </h1>
     </div>
 </div>
@@ -11,7 +11,7 @@
     <div class="col-lg-12">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title"><i class="fa fa-book fa-fw"></i> {{LANG['grids']}}</h3>
+                <h3 class="panel-title"><i class="fa fa-book fa-fw"></i> {{LANG['languages']}}</h3>
             </div>
             <div class="panel-body">
                 <div style="overflow-x: auto;" class="list-group">
