43bfe96286abbbbe42edbfd8bae2f8faccf283c0
Ozan UYKUN <znframework@gmail.com>
2018-04-28 12:23:58 +0300

    Updated config contents.

--- Config/Filesystem.php
+++ Config/Filesystem.php
@@ -1,62 +1,21 @@
 <?php return
 [
-    //--------------------------------------------------------------------------------------------------
-    // FileSystem
-    //--------------------------------------------------------------------------------------------------
-    //
-    // Author     : Ozan UYKUN <ozanbote@windowslive.com> | <ozanbote@gmail.com>
-    // Site       : www.znframework.com
-    // License    : The MIT License
-    // Copyright  : Copyright (c) 2012-2016, ZN Framework
-    //
-    //--------------------------------------------------------------------------------------------------
+   /*
+    |--------------------------------------------------------------------------
+    | File
+    |--------------------------------------------------------------------------
+    |
+    | It contains several settings for the File library.
+    |
+    | realPath: The methods of the File class specify whether to access 
+    |           the file relative to the absolute path or not.
+    | parentDirectoryAccess: Set whether to allow parent directory(../) access.
+    |
+    */
 
-    //--------------------------------------------------------------------------------------------------
-    // File
-    //--------------------------------------------------------------------------------------------------
-    //
-    // Genel Kullanım: Ftp bağlantı ayarları yapılır.
-    //
-    //--------------------------------------------------------------------------------------------------
     'file' =>
     [
-        //----------------------------------------------------------------------------------------------
-        // Real Path
-        //----------------------------------------------------------------------------------------------
-        //
-        // File ve Folder kütüphanelerinin kullanımında dosya işlemleri tam yolları ile mi yapılsın?
-        // Sorusuna yanıt vermek için oluşturulmuştur. Bu ayarın true olması durumunda dosyalara gerçek
-        // yolları ile erişim sağlanır. Aksi halde yalın yollar ile erişim sağlanır.
-        //
-        //----------------------------------------------------------------------------------------------
         'realPath'              => true,
-
-        //----------------------------------------------------------------------------------------------
-        // Parent Directory Access
-        //----------------------------------------------------------------------------------------------
-        //
-        // File ve Folder kütüphanelerinin kullanımında ../ sembolü ile bir üst dizine erişim sağlanıp
-        // sağlanayamacağıdır. Mevcut kütüphane güvenlik problemleri nedeni ile bu erişimi kapatmıştır.
-        // Bu durumundan kurtulmak için bu ayarın değerini true olarak ayarlayabilirsiniz.
-        //
-        //----------------------------------------------------------------------------------------------
         'parentDirectoryAccess' => false
-    ],
-
-    //--------------------------------------------------------------------------------------------------
-    // Ftp
-    //--------------------------------------------------------------------------------------------------
-    //
-    // Genel Kullanım: Ftp bağlantı ayarları yapılır.
-    //
-    //--------------------------------------------------------------------------------------------------
-    'ftp' =>
-    [
-        'host'       => '',   // Bağlantının sağlanacağı host bilgisi
-        'user'       => '',   // Sunucu kullanıcı adı.
-        'password'   => '',   // Sunucu kullanıcı şifresi.
-        'timeout'    => 90,   // Sunucu bağlantı zaman aşımı süresi.
-        'port'       => 21,   // Bağlantı port numarası.
-        'sslConnect' => false // SSL kullanılarak sunucu bağlantısı kurulsun mu?.
     ]
 ];
